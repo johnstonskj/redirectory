c4c13ac47831487d135ae4960a685d189c2d5dd9
Simon Johnston <simon@johnstonshome.org>
2014-03-28 15:48:12 -0700

    Better option handling, now also writes to a file

--- redirect.py
+++ redirect.py
@@ -12,19 +12,36 @@ class SmartRedirectHandler(urllib2.HTTPRedirectHandler):
 
     def http_error_301(self, req, fp, code, msg, headers):
         self.target = headers["Location"]
-        self.method = "301"
+        self.method = "HTTP 301"
         return None
 
     def http_error_302(self, req, fp, code, msg, headers):
         self.target = headers["Location"]
-        self.method = "302"
+        self.method = "HTTP 302"
         return None
 
 
-def process_host(host):
+def parse_options():
+    from optparse import OptionParser
+    
+    parser = OptionParser()
+    parser.add_option("-i", "--input", dest="input", 
+                      help="Input file, one URL per line")
+    parser.add_option("-o", "--output", dest="output", 
+                      help="Output file, as CSV")
+    parser.add_option("-p", "--protocol", dest="protocol", default="http",
+                      help="default protocol if not specified (http)")
+    parser.add_option("-t", "--gateway-title", dest="title", default="internet authorization",
+                      help="What page title indicates the authentication gateway page?")
+    (options, args) = parser.parse_args()
+    if not options.input and len(args) == 0:
+        parser.print_help()
+    return (options, args)
+
+def process_host(host, options):
     """ process either hostname or URL """
     if host[:4] != "http":
-        host = "http://" + host
+        host = "%s://%s" % (options.protocol, host)
     target = None
     method = None
     # Create request using http by default; if you want https you need to ask for it
@@ -43,7 +60,7 @@ def process_host(host):
                 index = target.find("url=")
                 if index >= 0:
                     target = target[index+4:]
-                    method = "meta tag"
+                    method = "META"
                     break
     except urllib2.HTTPError, err:
         if err.code == 301 or err.code == 302:
@@ -69,15 +86,23 @@ def process_host(host):
         # no redirection occured
         print "%s, %s, NO REDIRECT" % (host, host)
 
-def process_file(filename):
+def process_file(filename, options):
     """ process a file where each line is a host name or URL """
     with open(filename) as f:
         for line in f:
-            process_host(line.strip())
+            process_host(line.strip(), options)
 
-if len(sys.argv) == 2:
-    process_host(sys.argv[1])
-elif len(sys.argv) == 3 and sys.argv[1] == "-f":
-    process_file(sys.argv[2])
-else:
-    print "Usage: python redirect.py (hostname | -f filename)"
+(options, args) = parse_options()
+
+if options.output:
+    options.outputf = open(options.output, 'wt')
+
+if options.input:
+    process_file(options.input, options)
+for host in args:
+    process_host(host, options)
+
+try:
+    options.outputf.close()
+except AttributeError:
+    pass
