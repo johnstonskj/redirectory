40df176194677800486e5ca47a4b9eec247dbd5e
Simon Johnston <simon@johnstonshome.org>
2014-03-28 16:09:11 -0700

    Write to file complete. Better HTML parsing.

--- redirect.py
+++ redirect.py
@@ -1,7 +1,7 @@
 import re, socket, sys
 import urllib2, httplib
+from HTMLParser import HTMLParser
 
-RE_META = re.compile("\s*<meta\s+http-equiv\s*=\s*\"Refresh\"\s+content\s*=\s*\"(?P<target>[^\"]+)", re.IGNORECASE)
 MAX_TIMEOUT=5 # seconds
 
 class SmartRedirectHandler(urllib2.HTTPRedirectHandler): 
@@ -20,6 +20,34 @@ class SmartRedirectHandler(urllib2.HTTPRedirectHandler):
         self.method = "HTTP 302"
         return None
 
+class MyHTMLParser(HTMLParser):
+    def __init__(self):
+        self.target = None
+        self.title = ''
+        self.current = None
+        HTMLParser.__init__(self)
+
+    def handle_starttag(self, tag, attrs):
+        name = tag.lower()
+        if name == 'meta':
+            equiv = target = None
+            for (name, value) in attrs:
+                if name == 'http-equiv':
+                    equiv = value
+                elif name == 'content':
+                    target = value
+            if equiv == 'Refresh':
+                target = target.lower()
+                index = target.find("url=")
+                if index >= 0:
+                    self.target = target[index+4:]
+        elif name == 'title':
+            self.current = 'title'
+    def handle_endtag(self, tag):
+        self.current = None
+    def handle_data(self, data):
+        if self.current == 'title':
+            self.title = self.title + data
 
 def parse_options():
     from optparse import OptionParser
@@ -50,18 +78,12 @@ def process_host(host, options):
     opener = urllib2.build_opener(handler)
     try:
         f = opener.open(request, timeout=MAX_TIMEOUT)
-        content = f.readlines()
-        # Look for HTML redirect in meta tags, this wont be needed on an HTTP redirect
-        for line in content:
-            # generally the content looks like n;url=<value> where n is the delay in seconds
-            match = RE_META.match(line)
-            if match != None:
-                target = match.group("target").lower()
-                index = target.find("url=")
-                if index >= 0:
-                    target = target[index+4:]
-                    method = "META"
-                    break
+        content = f.read()
+        parser = MyHTMLParser()
+        parser.feed(content)
+        if not parser.target is None:
+            target = parser.target
+            method = 'META'
     except urllib2.HTTPError, err:
         if err.code == 301 or err.code == 302:
             # we only process these standard redirects
@@ -79,12 +101,13 @@ def process_host(host, options):
         # anything else ends up here
         err = sys.exc_info()[0]
         target = "ERROR processing %s: %s" % (host, str(err))
+        raise
     if not target is None:
         # a redirect, or error
-        print "%s, %s, %s" % (host, target, method)
+        print >> options.outputf, "%s, %s, %s" % (host, target, method)
     else:
         # no redirection occured
-        print "%s, %s, NO REDIRECT" % (host, host)
+        print >> options.outputf, "%s, %s, NO REDIRECT" % (host, host)
 
 def process_file(filename, options):
     """ process a file where each line is a host name or URL """
@@ -96,13 +119,13 @@ def process_file(filename, options):
 
 if options.output:
     options.outputf = open(options.output, 'wt')
+else:
+    options.outputf = sys.stdout
 
 if options.input:
     process_file(options.input, options)
 for host in args:
     process_host(host, options)
 
-try:
+if options.output:
     options.outputf.close()
-except AttributeError:
-    pass
